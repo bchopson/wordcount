da68e69e5db0e9807f11b504d58b0a531475bbc8
Ben Chopson <bjamin513@gmail.com>
2017-01-02 19:03:15 +0000

    UI improvements
    * Loading spinner
    * Friendly error message

--- static/main.js
+++ static/main.js
@@ -7,6 +7,10 @@
   .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
     function ($scope, $log, $http, $timeout) {
 
+      $scope.submitButtonText = 'Submit';
+      $scope.loading = false;
+      $scope.urlerror = false;
+
       $scope.getResults = function () {
 
         var userInput = $scope.url;
@@ -15,6 +19,10 @@
           success(function (results) {
             $log.log(results);
             getWordCount(results);
+            $scope.wordcounts = null;
+            $scope.loading = true;
+            $scope.urlerror = false;
+            $scope.submitButtonText = 'Loading...';
           }).
           error(function (error) {
             $log.log(error);
@@ -31,12 +39,20 @@
                 $log.log(data, status);
               } else if (status === 200) {
                 $log.log(data);
+                $scope.loading = false;
+                $scope.submitButtonText = 'Submit';
                 $scope.wordcounts = data;
                 $timeout.cancel(timeout);
                 return false;
               }
 
               timeout = $timeout(poller, 2000);
+            }).
+            error(function (error) {
+              $log.log(error);
+              $scope.loading = false;
+              $scope.submitButtonText = 'Submit';
+              $scope.urlerror = true;
             });
         };
 
